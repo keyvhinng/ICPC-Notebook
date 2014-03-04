cc1cfe29bf5d60e12fc45720bb601981b4bd82b0
keyvhinng <keyvhinng@me.com>
2014-03-04 17:56:22 -0300

    first commit

---
+++ template.cpp
@@ -0,0 +1,36 @@
+//Author: Keyvhinng Espinoza
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <bitset>
+#include <cmath>
+#include <ctime>
+
+#define REP(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
+#define snuke(c,itr) for(__typeof((c).begin()) itr=(c).begin(); it!=(c).end(); itr++)
+#define f(i,x,y) for(int i=x; i<y; i++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define debug(x) {cerr <<#x << " = " <<x <<endl; }
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
